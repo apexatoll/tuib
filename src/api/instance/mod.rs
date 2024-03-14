dd425602e7b36f52645fb571796f0cd9bb7e46ed
Chris Welham <71787007+apexatoll@users.noreply.github.com>
2024-03-14 01:12:32 +0000

    Implement Instance::from<&str>()

--- src/api/instance/mod.rs
+++ src/api/instance/mod.rs
@@ -5,3 +5,11 @@ mod fetcher;
 pub struct Instance {
     url: Url,
 }
+
+impl From<&str> for Instance {
+    fn from(value: &str) -> Self {
+        let url = Url::parse(value).unwrap();
+
+        Self { url }
+    }
+}

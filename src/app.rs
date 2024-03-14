40ba27755acd99140a637ecbeadc86aeb5cb1f10
Chris Welham <71787007+apexatoll@users.noreply.github.com>
2024-03-14 03:10:25 +0000

    Implement App::setup()

--- src/app.rs
+++ src/app.rs
@@ -7,3 +7,17 @@ pub struct App {
     client: Client,
     instance: Instance,
 }
+
+impl App {
+    const SOURCE: &'static str = "https://api.invidious.io";
+
+    pub async fn setup() -> Result<Self> {
+        let client = Client::new();
+        let instance = Instance::fetch(Self::SOURCE, &client).await?;
+
+        Ok(Self {
+            instance,
+            client,
+        })
+    }
+}

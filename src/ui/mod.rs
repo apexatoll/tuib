decf4fb1e764635f91b129eedf6b74eb8656abc0
Chris Welham <71787007+apexatoll@users.noreply.github.com>
2024-03-14 21:58:41 +0000

    Add UI test helper macros

--- src/ui/mod.rs
+++ src/ui/mod.rs
@@ -5,3 +5,33 @@ use routers::Router;
 
 mod handlers;
 use handlers::StatefulHandler;
+
+mod test_helpers {
+    macro_rules! assert_buffer {
+        ($component:expr, $buffer:expr, $state:expr) => {
+            let backend = ratatui::backend::TestBackend::new(
+                $buffer.area.width,
+                $buffer.area.height,
+            );
+
+            let mut terminal = Terminal::new(backend).unwrap();
+
+            terminal.draw(|frame|
+                frame.render_stateful_widget($component, frame.size(), $state)
+            ).unwrap();
+
+            terminal.backend().assert_buffer($buffer);
+        }
+    }
+
+    macro_rules! handle_messages {
+        ($component:expr, $messages:expr, $state:expr) => {
+            for message in $messages.into_iter() {
+                $component.handle(message, $state).await.unwrap();
+            }
+        }
+    }
+
+    pub(super) use assert_buffer;
+    pub(super) use handle_messages;
+}

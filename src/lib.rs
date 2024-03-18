f2f091c2f3bf3973a6fef370ed3dbec632bcaad2
Chris Welham <71787007+apexatoll@users.noreply.github.com>
2024-03-18 01:07:47 +0000

    Define library exports

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,10 @@
 #![allow(unused)]
 
-type Result<T> = std::result::Result<T, Box<dyn std::error::Error>>;
+pub type Result<T> = std::result::Result<T, Box<dyn std::error::Error>>;
+pub use app::App;
+pub use crossterm::event::{self, *};
+pub use ratatui::{prelude::*, widgets::*};
 
 mod api;
 mod app;
 mod ui;
-
-use app::App;
-use crossterm::event::{self, *};
-use ratatui::{prelude::*, widgets::*};
